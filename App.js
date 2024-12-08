7e3a647abff33e9125c13d33a99f6770626c1f34
dayXX27 <the.yunglegend10@gmail.com>
2024-12-07 23:00:20 -0300

    Update

--- App.js
+++ App.js
@@ -1,34 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, FlatList} from 'react-native';
+import { useState } from 'react';
+import { Text, View, TextInput, TouchableOpacity, FlatList} from 'react-native';
 
-const API_KEY = "ae0050c3ef5865c95d6982924c7de685"
-const URL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="
-const PARAMS = `&api_key=${API_KEY}&format=json&limit=10`
+const API_KEY = "5d2df7527e3aae5e2ce3b250fe868046"
+const URL = "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="
+const PARAMS = `&api_key=${API_KEY}&format=json&limit=5`
 
 export default function App() {
     const [artistInput, setArtistInput] = useState('');
     const [artistData, setArtistData] = useState([]);
 
-    // useEffect(() => {
-    // })
     const fetchArtist = async () => {
         const response = await fetch(URL + artistInput + PARAMS)
         const data = await response.json()
-        setArtistData(data.toptracks.track)
-        console.log(data.toptracks.track)
+        setArtistData(data.topalbums.album)
+        console.log(data.topalbums.album)
     }
     
     return (
-        <View className="flex-1 bg-white justify-center p-8 gap-2">
-            <Text className="text-2xl font-roboto font-bold">Procurar artista</Text>
+        <View className="flex-1 bg-gradient-to-t from-purple-600 to-blue-600 justify-center p-8 gap-2">
+            <Text className="text-2xl font-bold">Procure um artista</Text>
             <TextInput 
                 value={artistInput}
                 onChangeText={setArtistInput}
                 placeholder='Digite o nome de um artista'
                 className="border border-slate-200 rounded-lg px-4 py-2"
             />
-            <TouchableOpacity className="p-4 bg-blue-600 rounded-lg items-center" onPress={fetchArtist}>
+            <TouchableOpacity className="p-4 bg-green-400 rounded-lg items-center" onPress={fetchArtist}>
                 <Text className="text-white font-bold">
                     Procurar
                 </Text>
@@ -37,11 +35,10 @@ export default function App() {
             <View>
                 <FlatList
                     data={artistData}
-                    // keyExtractor={item => item.id}
                     renderItem={({item}) => 
                         <View>
-                            <Text>Nome da música <Text className="font-bold">{item.name}</Text></Text>
-                            <Text>Ouvintes <Text className="font-bold">{item.listeners}</Text></Text>
+                            <Text>Nome do album: <Text className="font-bold">{item.name}</Text></Text>
+                            <Text>Plays: <Text className="font-bold">{item.playcount}</Text></Text>
                         </View>
                     }
                     ItemSeparatorComponent={() => <View style={{height: 10}} />}
